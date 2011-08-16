e074bcf385f6b978935c89c060d357ca5efc8f9d
c4kofony@gmail.com <c4kofony@gmail.com@e50f07f0-6b2f-0410-b017-1367fa090f66>
2011-08-16 14:46:13 +0000

    fixed licensing

--- FLOSSmoleTigris/tgLists.py
+++ FLOSSmoleTigris/tgLists.py
@@ -1,3 +1,26 @@
+# Carter Kozak
+# c4kofony@gmail.com
+# ckozak@elon.edu
+
+#collector/parser for Tigris mailing list data
+
+# flossmole.org
+
+# Copyright (C) 2011 Carter Kozak
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sqlalchemy
 from sqlalchemy import *
 import urllib
